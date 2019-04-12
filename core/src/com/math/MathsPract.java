89b8b16ad0763ff15ff466baf5b37763d03831b6
singhnavin19 <singhnavin19290@gmail.com>
2019-04-12 20:34:59 +0530

    math and web

---
+++ core/src/com/math/MathsPract.java
@@ -0,0 +1,20 @@
+package com.math;
+
+public class MathsPract {
+
+	public static void main(String[] args) {
+
+		Double res = Math.pow(3, 4);
+		System.out.println(res);
+		String s = "abcvd";
+		System.out.println(s.indexOf('c'));
+
+		int[] arr = new int[5];
+		arr[0] = 10;
+
+		System.out.println(arr[0]);
+		char x = 'y';
+
+	}
+
+}
